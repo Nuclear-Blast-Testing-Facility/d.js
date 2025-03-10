d8ea2c6dca73a1d406e4af7b1d8f9dbc4ccc1540
cbgames-xyz <106137185+EatMYSHOORTS@users.noreply.github.com>
2025-03-11 11:25:03 +1300

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,56 @@
+require('dotenv').config();
+const { Client, IntentsBitField} = require('discord.js');
+
+const client = new Client({
+    intents: [
+      IntentsBitField.Flags.Guilds,
+      IntentsBitField.Flags.GuildMembers,
+      IntentsBitField.Flags.GuildMessages,
+      IntentsBitField.Flags.MessageContent,  
+    ]
+});
+
+client.on('ready', (c) => {
+    console.log(`✔ ${c.user.tag} is online.`)
+});
+
+client.on('messageCreate', (message) => {
+    if (message.author.bot) {
+        return;}
+    
+    if (message.content === 'hello') {
+      message.reply('hello');  
+    }
+})
+
+client.on('interactionCreate', (interaction) => {
+  if (interaction.isChatInputCommand())
+  
+  if (interaction.commandName === 'hey' ) {
+    interaction.reply('hey!');
+  }
+    if (interaction.commandName === 'ping' ) {
+        interaction.reply('Pong!');
+  }
+  if (interaction.commandName === 'pong' ) {
+    interaction.reply('Pigit remote add originng!');
+}
+  if (interaction.commandName === 'add' ) {
+    const num1 = interaction.options.get('first-number').value;
+    const num2 = interaction.options.get('second-number').value;
+    interaction.reply(`The sum is ${num1 + num2}`);
+    console.log('added numbers');
+}
+if (interaction.commandName === 'times' ) {
+  const num1 = interaction.options.get('first-number').value;
+  const num2 = interaction.options.get('second-number').value;
+  interaction.reply(`The sum is ${num1 * num2}`);
+  console.log('timed numbers');
+}
+if (interaction.commandName === 'console-log') {
+  const message = interaction.options.get('message').value;
+  interaction.reply(`Sending "${message}" to Console Logs`);
+  console.log(`"${message}" Sent From ${interaction.user.tag}`);
+}
+});
+client.login(process.env.TOKEN);
---
+++ src/register-commands.js
@@ -0,0 +1,88 @@
+require('dotenv').config();
+const {REST, Routes, ApplicationCommandOptionType} = require('discord.js');
+
+const commands = [
+    {
+        name: 'hey',
+        description: 'Replies with Hey!',
+    },
+    {
+        name: 'console-log',
+        description: 'sends message to bot dev console logs',
+        options: [{
+            name: 'message',
+            description: 'Send a message!',
+            type: ApplicationCommandOptionType.String,
+            required: true,
+        }]
+    },
+    {
+        name: 'ping',
+        description: 'Pong!',
+    },
+    {
+        name: 'pong',
+        description: 'Ping!',
+    },
+    {
+        name: 'times',
+        description: 'Num X Num Calculator',
+        options: [
+            {
+                name: 'first-number',
+                description: 'The first number.',
+                type: ApplicationCommandOptionType.Number,
+                required: true,
+            },
+            {
+                name: 'second-number',
+                description: 'The second number.',
+                type: ApplicationCommandOptionType.Number,
+                required: true,
+            },]
+    },
+    {
+      name : 'add',
+      description : 'Addition Calculator',
+      options: [
+        {
+            name: 'first-number',
+            description: 'The first number.',
+            type: ApplicationCommandOptionType.Number,
+            choices: [
+            {
+                name: 'one',
+                value: 1,
+            },
+            {
+                name: 'two',
+                value: 2,
+            }
+            ],
+            required: true,
+        },
+        {
+            name: 'second-number',
+            description: 'The second number.',
+            type: ApplicationCommandOptionType.Number,
+            required: true,
+        },
+      ]
+    },
+];
+
+const rest = new REST({version: '10'}).setToken(process.env.TOKEN);
+
+(async () => {
+    try {
+console.log('Registering slash commands...') 
+      await rest.put(
+        Routes.applicationGuildCommands(process.env.CLIENT_ID, process.env.GUILD_ID),
+        { body: commands }
+      ) 
+
+      console.log('Slash Commands working.')
+    } catch (error) {
+        console.log(`ErRoR: ${error}`)
+    }
+})();
